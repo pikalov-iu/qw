af278c85506fed98eb537d1a73dc3ad710e73fa0
Jucutu <108612341+Jucutu@users.noreply.github.com>
2022-11-17 17:14:18 +0300

    Create ui.py

---
+++ ui.py
@@ -0,0 +1,42 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(576, 544)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(10, 50, 531, 431))
+        self.label.setText("")
+        self.label.setObjectName("label")
+        self.horizontalLayoutWidget = QtWidgets.QWidget(self.centralwidget)
+        self.horizontalLayoutWidget.setGeometry(QtCore.QRect(9, 10, 211, 25))
+        self.horizontalLayoutWidget.setObjectName("horizontalLayoutWidget")
+        self.horizontalLayout = QtWidgets.QHBoxLayout(self.horizontalLayoutWidget)
+        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.drawPushButton = QtWidgets.QPushButton(self.horizontalLayoutWidget)
+        self.drawPushButton.setObjectName("drawPushButton")
+        self.horizontalLayout.addWidget(self.drawPushButton)
+        self.cleanPushButton = QtWidgets.QPushButton(self.horizontalLayoutWidget)
+        self.cleanPushButton.setObjectName("cleanPushButton")
+        self.horizontalLayout.addWidget(self.cleanPushButton)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 576, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.drawPushButton.setText(_translate("MainWindow", "Сделать круг"))
+        self.cleanPushButton.setText(_translate("MainWindow", "Очистить")
